1f2fbf3fc423a33122fb2ffbfcc2a77912b097ec
high1 <high1@mailfence.com>
2022-08-07 23:08:32 +0200

    refactor: used new esbuild jsx automatic runtime support

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'tsup';
-import voby from 'voby-esbuild';
 
 export default defineConfig({
   clean: true,
   dts: true,
   entry: ['src/index.tsx'],
-  esbuildPlugins: [voby()],
   format: ['esm'],
   name: 'voby-router',
 });
